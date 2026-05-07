29751ff523e617d194e45c7d40e636c62e44f82d
Héctor Santana <hector.santana.gc@gmail.com>
2026-05-07 18:50:19 +0100

    Limpiar código, declare strict innecesarios

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Configuration constants for the application
  */
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 // Load configuration
 $config = require __DIR__ . '/includes/constants.php';
 
